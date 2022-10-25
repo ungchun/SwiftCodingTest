4c54de9ffc3ae43dfebd42de5a1e0ffaaffcebf4
ungchun <leedool3003@naver.com>
2022-10-25 12:40:31 +0900

    solved : 2775

---
+++ CT_CommandLineTool/Baekjoon/Bronze/B1_2775.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+let T = Int(readLine()!)!
+for _ in 1...T {
+    let k = Int(readLine()!)!
+    let n = Int(readLine()!)!
+    var arr = Array(repeating: Array(repeating: 0, count: n + 1), count: k + 1)
+
+    for i in 0...k {
+        for j in 0...n {
+            if i == 0 {
+                arr[i][j] = j + 1
+
+            }else{
+                if j == 0 {
+                    arr[i][j] = 1
+                }else {
+                    arr[i][j] = arr[i][j - 1] + arr[i - 1][j]
+
+                }
+            }
+        }
+    }
+    print("\(arr[k][n - 1])")
+}
