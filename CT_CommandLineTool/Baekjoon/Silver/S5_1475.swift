e9c569e78c02368caed822cb948775ab240c2e10
ungchun <leedool3003@naver.com>
2022-11-01 14:37:57 +0900

    solved : 1475

--- CT_CommandLineTool/Baekjoon/Silver/S5_1475.swift
+++ CT_CommandLineTool/Baekjoon/Silver/S5_1475.swift
@@ -1,16 +1,79 @@
 import Foundation
 
-let input = readLine()!
-var num = Array(repeating: 0, count: 10)
+var N = Array(readLine()!).map {Int(String($0))}
 
-for i in input {
-    num[Int(String(i))!] += 1
+var sixCheck = 0
+var result = 0
+
+var arrayDict: [[Int: Bool]] = [[:]]
+var numSet: [Int: Bool] = [:]
+numSet[0] = false
+numSet[1] = false
+numSet[2] = false
+numSet[3] = false
+numSet[4] = false
+numSet[5] = false
+numSet[6] = false
+numSet[7] = false
+numSet[8] = false
+numSet[9] = false
+
+arrayDict[0] = numSet
+result += 1
+
+N.forEach { value in
+    if value == 6 {
+        var addCheck = true
+        for i in 0..<arrayDict.count {
+            if arrayDict[i][value!] == false {
+                arrayDict[i][value!] = true
+                addCheck = false
+                break
+            } else if arrayDict[i][9] == false {
+                arrayDict[i][9] = true
+                addCheck = false
+                break
+            }
+        }
+        if addCheck {
+            result += 1
+            arrayDict.append(numSet)
+            arrayDict[arrayDict.count-1][value!] = true
+        }
+    } else if value == 9 {
+        var addCheck = true
+        for i in 0..<arrayDict.count {
+            if arrayDict[i][value!] == false {
+                arrayDict[i][value!] = true
+                addCheck = false
+                break
+            } else if arrayDict[i][6] == false {
+                arrayDict[i][6] = true
+                addCheck = false
+                break
+            }
+        }
+        if addCheck {
+            result += 1
+            arrayDict.append(numSet)
+            arrayDict[arrayDict.count-1][value!] = true
+        }
+        
+    } else {
+        var addCheck = true
+        for i in 0..<arrayDict.count {
+            if arrayDict[i][value!] == false {
+                arrayDict[i][value!] = true
+                addCheck = false
+                break
+            }
+        }
+        if addCheck {
+            result += 1
+            arrayDict.append(numSet)
+            arrayDict[arrayDict.count-1][value!] = true
+        }
+    }
 }
 
-num[9] += num[6]
-num[6] = 0
-
-if num.firstIndex(of: num.max()!) == 9 {
-    print((num[9] / 2) + (num[9] % 2))
-}
-else { print(num.max()!) }
+print(result)
