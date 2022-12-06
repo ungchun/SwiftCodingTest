bc268e471c3022000c432c7fdd25c54f43fa0c12
ungchun <leedool3003@gmail.com>
2022-12-06 12:53:24 +0900

    solved : 예상 대진표_2

---
+++ CT_CommandLineTool/프로그래머스/Lv2_예상 대진표_2.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+func solution(_ n:Int, _ a:Int, _ b:Int) -> Int
+{
+    var copyA = a
+    var copyB = b
+    var result = 1
+    
+    var check = copyA > copyB ? copyA : copyB
+    
+    while copyA < copyB ? (copyA+1 != copyB || check % 2 != 0) : (copyB+1 != copyA || check % 2 != 0) {
+        if copyA % 2 == 0 {
+            copyA /= 2
+        } else {
+            copyA = (copyA+1)/2
+        }
+        
+        if copyB % 2 == 0 {
+            copyB /= 2
+        } else {
+            copyB = (copyB+1)/2
+        }
+        
+        print("copyA \(copyA)")
+        print("copyB \(copyB)")
+        
+        check = copyA > copyB ? copyA : copyB
+        
+        result += 1
+    }
+    
+    return result
+}
+
+print(solution(8, 4, 5))
