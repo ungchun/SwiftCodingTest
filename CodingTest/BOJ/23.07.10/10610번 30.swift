109a0e2b26dc2a19039cb55eefcb34d79907c25b
sunghun <leedool3003@gmail.com>
2023-07-10 09:46:39 +0900

    [BOJ] 10610번 30

---
+++ CodingTest/BOJ/23.07.10/10610번 30.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+var nums = Array(readLine()!).sorted(by: >)
+var input = nums.reduce(0) {Int(String($0))! + Int(String($1))!}
+if nums.last != "0" || input % 3 != 0 {
+	print("\(-1)")
+} else {
+	print("\(String(nums))")
+}
