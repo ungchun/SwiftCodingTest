ffd13b60e69964e64a76b7ea52923fa7f17fa1de
sunghun <leedool3003@gmail.com>
2023-08-08 21:53:16 +0900

    [Programmers] 이차원 배열 대각선 순회하기

---
+++ CodingTest/Programmers/23.08.08/이차원 배열 대각선 순회하기.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ board:[[Int]], _ k:Int) -> Int {
+	
+	var result = 0
+	
+	for i in 0..<board.count {
+		for j in 0..<board[i].count {
+			if i + j > k {
+				continue
+			} else {
+				result += board[i][j]
+			}
+		}
+	}
+	
+	return result
+}
