26af857e22729bf8e579f8566726e15b75f90b5f
sunghun <leedool3003@gmail.com>
2023-08-17 23:31:09 +0900

    [Programmers] 문자열 반복해서 출력하기

---
+++ CodingTest/Programmers/23.08.17/문자열 반복해서 출력하기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let inp = readLine()!.components(separatedBy: [" "]).map { $0 }
+let (s1, a) = (inp[0], Int(inp[1])!)
+
+var result = ""
+
+for _ in 0..<a {
+	result += s1
+}
+
+print(result)
