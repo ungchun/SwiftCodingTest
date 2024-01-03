f1b4e60998a8b23553a244bdef4d54fc64d5e399
sunghun <leedool3003@gmail.com>
2024-01-03 15:57:13 +0900

    [BOJ] 14888번 연산자 끼워넣기

---
+++ CodingTest/BOJ/24.01.03/14888번 연산자 끼워넣기.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let array = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+// + - * /
+var operators = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+var maxVal = Int.min
+var minVal = Int.max
+
+calculate(array.first!, 1)
+
+print(maxVal)
+print(minVal)
+
+// 메소드 선언
+func calculate(_ value: Int, _ index: Int) {
+		if index == n {
+				maxVal = max(value, maxVal)
+				minVal = min(value, minVal)
+				return
+		}
+		
+		for i in 0 ..< 4 {
+				if operators[i] == 0 {
+						continue
+				}
+
+				operators[i] -= 1
+
+				var result = value
+
+				switch i {
+				case 0:
+						result += array[index]
+				case 1:
+						result -= array[index]
+				case 2:
+						result *= array[index]
+				case 3:
+						result /= array[index]
+				default:
+						break
+				}
+
+				calculate(result, index + 1)
+				operators[i] += 1
+		}
+}
