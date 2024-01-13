fd8b16e424bee0d24ef6c042b7340587b17cb21b
sunghun <leedool3003@gmail.com>
2024-01-13 19:39:00 +0900

    [BOJ] 15650번 N과 M (2)

---
+++ CodingTest/BOJ/24.01.13/15650번 N과 M (2).swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = nm[0], m = nm[1]
+
+var visited = Array(repeating: false, count: n + 1)
+var depth: Int = 0
+var stack: [Int] = []
+
+func dfs(_ depth: Int) {
+	if depth == m {
+		if stack.sorted() == stack {
+			print(stack.map{String($0)}.joined(separator: " "))
+			return
+		}
+		return
+	}
+	
+	for i in 1...n {
+		if !visited[i] {
+			visited[i] = true
+			stack.append(i)
+			dfs(depth+1)
+			visited[i] = false
+			stack.removeLast()
+		}
+	}
+}
+
+dfs(0)
