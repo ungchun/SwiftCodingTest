95e33f82147a62d3248537d83b3eff01dd110145
sunghun <leedool3003@gmail.com>
2024-01-13 19:39:52 +0900

    [BOJ] 15664번 N과 M (10)

---
+++ CodingTest/BOJ/24.01.13/15664번 N과 M (10).swift
@@ -0,0 +1,31 @@
+import Foundation
+
+let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = nm[0], m = nm[1]
+var values = readLine()!.split(separator: " ").map{Int(String($0))!}.sorted()
+var visited = Array(repeating: false, count: n)
+var stack: [String] = []
+var check: Set<String> = []
+var str = ""
+
+func dfs(_ start: Int) {
+	if stack.count == m {
+		let value = stack.joined(separator: " ")
+		if !check.contains(value) {
+			check.insert(value)
+			print(value)
+		}
+		return
+	}
+	
+	for i in start..<values.count {
+		if !visited[i] {
+			visited[i] = true
+			stack.append(String(values[i]))
+			dfs(i)
+			stack.removeLast()
+			visited[i] = false
+		}
+	}
+}
+dfs(0)
