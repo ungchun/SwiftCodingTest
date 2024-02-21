3d6150be394d31266efe699cb5a93f9970ef2e1c
sunghun <leedool3003@gmail.com>
2024-02-21 21:38:35 +0900

    [BOJ] 12865번 평범한 배낭

---
+++ CodingTest/BOJ/24.02.21/12865번 평범한 배낭.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let nk = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = nk[0], k = nk[1]
+var dp = Array(repeating: Array(repeating: 0, count: k+1), count: n)
+
+for i in 0..<n {
+	let line = readLine()!.split(separator: " ").map{Int(String($0))!}
+	for j in 1...k {
+		if i == 0 {
+			if line[0] <= j {
+				dp[i][j] = line[1]
+			}
+		} else {
+			if j >= line[0] {
+				dp[i][j] = max(dp[i-1][j], line[1]+dp[i][j-line[0]])
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+}
+print(dp)
