693d9d8d79a180abb65008962cefc207b254109d
sunghun <leedool3003@gmail.com>
2024-02-25 17:54:52 +0900

    [BOJ] 2529번 부등호

---
+++ CodingTest/BOJ/24.02.25/2529번 부등호.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+let k = Int(readLine()!)!
+var arr = readLine()!.split(separator: " ").map{String($0)}
+var answer = [String]()
+var visited = Array(repeating: false, count: 10)
+var resultArr = [Int]()
+
+func dfs(_ depth: Int, _ now: Int) {
+	if depth == k+1 {
+		answer.append(resultArr.map{String($0)}.joined())
+		return
+	}
+	
+	for i in 0...9 {
+		if !visited[i] {
+			if depth > 0 {
+				if now >= i && arr[depth-1] == "<" {continue}
+				else if now <= i && arr[depth-1] == ">" {continue}
+			}
+			
+			visited[i] = true
+			resultArr.append(i)
+			dfs(depth+1, i)
+			visited[i] = false
+			resultArr.removeLast()
+		}
+	}
+}
+
+dfs(0, 0)
+print(answer.max()!)
+print(answer.min()!)
